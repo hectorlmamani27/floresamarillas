b2f4e5079abd622908bf1a227066a84d75becdc6
hectorlmamani27 <145698562+hectorlmamani27@users.noreply.github.com>
2023-09-21 11:24:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// JavaScript para generar tulipanes amarillos de forma diseminada
-function crearTulipan() {
-    const tulipan = document.createElement('img');
-    tulipan.src = 'imagen-tulipan.jpg'; // Reemplaza con la URL de tu imagen de tulipán
-    tulipan.style.width = '50px'; // Ajusta el tamaño según tus preferencias
-    tulipan.style.position = 'absolute';
-    tulipan.style.top = `${Math.random() * window.innerHeight}px`;
-    tulipan.style.left = `${Math.random() * window.innerWidth}px`;
-    document.getElementById('tulipanes').appendChild(tulipan);
+// JavaScript para mostrar una imagen que aparece lentamente desde una sombra
+const imagen = document.getElementById('imagen');
+
+function aparecerImagen() {
+    imagen.style.opacity = 1; // Cambia la opacidad para mostrar la imagen gradualmente
 }
 
-setInterval(crearTulipan, 2000); // Crea un tulipán cada 2 segundos
+setTimeout(aparecerImagen, 2000); // Espera 2 segundos antes de mostrar la imagen
+
