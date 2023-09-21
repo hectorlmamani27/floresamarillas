13e047ad653b4d45987f692bb71eb995a4dcb7fd
hectorlmamani27 <145698562+hectorlmamani27@users.noreply.github.com>
2023-09-21 12:06:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-// JavaScript para mostrar una imagen que aparece lentamente desde una sombra
+// JavaScript para mostrar una imagen con efecto "fade in"
 const imagen = document.getElementById('imagen');
 
 function aparecerImagen() {
-    imagen.style.opacity = 0.2; // Cambia la opacidad para mostrar la imagen gradualmente
+    let opacity = 0;
+    const fadeInInterval = setInterval(function () {
+        if (opacity < 1) {
+            opacity += 0.01; // Ajusta el valor de incremento según la velocidad de desvanecimiento
+            imagen.style.opacity = opacity;
+        } else {
+            clearInterval(fadeInInterval); // Detiene la animación cuando la opacidad llega a 1
+        }
+    }, 10); // Intervalo de tiempo para la animación (en milisegundos)
 }
 
-setTimeout(aparecerImagen, 1000); // Espera 2 segundos antes de mostrar la imagen
-
+setTimeout(aparecerImagen, 2000); // Espera 2 segundos antes de mostrar la imagen
