04bec836faa35f0365b4a56824477e7fdf169fd1
hectorlmamani27 <145698562+hectorlmamani27@users.noreply.github.com>
2023-09-21 11:43:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 const imagen = document.getElementById('imagen');
 
 function aparecerImagen() {
-    imagen.style.opacity = 2; // Cambia la opacidad para mostrar la imagen gradualmente
+    imagen.style.opacity = 10; // Cambia la opacidad para mostrar la imagen gradualmente
 }
 
-setTimeout(aparecerImagen, 2000); // Espera 2 segundos antes de mostrar la imagen
+setTimeout(aparecerImagen, 1000); // Espera 2 segundos antes de mostrar la imagen
 
